7b17aae9e114a63034c6c33b1b6f81ed8c35834e
paulsanswrk <paulsanswrk@gmail.com>
2023-05-29 17:21:24 +0600

    Revert last change

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import {Inspector} from '@babylonjs/inspector';
 
 class SpiralView implements ISpiralParams {
 
-    curr_n = 16;
+    curr_n = 12;
     new_n = this.curr_n;
     do_transition = false;
     spirals = Spiral_Top.all_configs.map((s, n) => new Spiral_Top(n));
@@ -33,15 +33,11 @@ class SpiralView implements ISpiralParams {
         this.scene);
 
     manager = new BABYLON.MorphTargetManager();
-    // meshes: Mesh[] = [];
-    // targets: MorphTarget[] = [];
+    meshes: Mesh[] = [];
+    targets: MorphTarget[] = [];
     spiralMaterial = new StandardMaterial("spiralMaterial", this.scene);
-    // randomColor = Color3.White();
-    curr_mesh: Mesh = this.create_spiral_mesh(this.curr_n, this.spirals[this.curr_n]);
-    new_mesh: Mesh;
-
-    curr_target: MorphTarget;
-    new_target: MorphTarget;
+    randomColor = Color3.White();
+    curr_mesh = this.create_spiral_mesh(this.curr_n, this.spirals[this.curr_n]);
 
     // bulbLight = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 0, 15), this.scene);
 // bulbLight.diffuse = randomColor
@@ -66,8 +62,6 @@ class SpiralView implements ISpiralParams {
         this.camera.autoRotationBehavior.idleRotationWaitTime = 0;
         this.camera.autoRotationBehavior.zoomStopsAnimation = false;
 
-        this.camera.inertia = 0;
-
         /*this.spiralMaterial.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
         this.spiralMaterial.emissiveColor = this.randomColor;*/
 
@@ -76,8 +70,6 @@ class SpiralView implements ISpiralParams {
         this.engine.runRenderLoop(() => {
             this.scene.render();
         });
-
-        // this.manager.
 
         // this.gui.init();
         this.scene.onPointerUp = (evt, pickInfo) => this.click_handler(evt, pickInfo);
@@ -93,7 +85,7 @@ class SpiralView implements ISpiralParams {
     }
 
 
-    create_spiral_mesh(n, spiral: Spiral_Top, enabled: boolean = true) {
+    create_spiral_mesh(n, spiral: Spiral_Top) {
         const mesh: Mesh = MeshBuilder.CreateTube(`spiral_${n}`, {
             path: spiral.spiralPoints,
             radius: 0.006,
@@ -101,31 +93,23 @@ class SpiralView implements ISpiralParams {
             tessellation: 4,
         }, this.scene);
 
-        // this.meshes[n] = mesh;
+        this.meshes[n] = mesh;
 
         mesh.material = this.spiralMaterial;
         this.spiralMaterial.specularColor = Color3.Black();
         mesh.freezeNormals();
 
+        mesh.setEnabled(n == this.curr_n);
         // if (n == this.curr_n)
         mesh.setVerticesData("color", spiral.calc_colors(mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind)));
-        // this.targets[n] = MorphTarget.FromMesh(mesh, `spiral_${n}`, 0);
-        // this.manager.addTarget(this.targets[n]);
+        mesh.morphTargetManager = this.manager;
+        this.targets[n] = BABYLON.MorphTarget.FromMesh(mesh, `spiral_${n}`, 0);
+        this.manager.addTarget(this.targets[n]);
 
-        const meshes = [mesh];
+        for (let n = 1; n < spiral.nRot; n++)
+            mesh.clone().rotate(new Vector3(0, 0, 1), 2 * Math.PI / spiral.nRot * n).name = `${mesh.name} rot ${n}`;
 
-        for (let n = 1; n < spiral.nRot; n++) {
-            const clone = mesh.clone();
-            clone.rotate(new Vector3(0, 0, 1), 2 * Math.PI / spiral.nRot * n);
-            clone.name = `${mesh.name} rot ${n}`;
-            meshes.push(clone);
-        }
-
-        const merged_mesh = BABYLON.Mesh.MergeMeshes(meshes, undefined, true, undefined, true, undefined);
-        merged_mesh.morphTargetManager = this.manager;
-        merged_mesh.setEnabled(enabled);
-
-        return merged_mesh;
+        return mesh;
     }
 
     render_handler() {
@@ -134,29 +118,25 @@ class SpiralView implements ISpiralParams {
         if (this.do_transition) {
             let finished = true;
 
-            if (Math.abs(this.new_target.influence - 1) < di)
-                this.new_target.influence = 1;
-            else {
-                this.new_target.influence += di;
-                finished = false;
-            }
-
-            if (Math.abs(this.curr_target.influence) < di)
-                this.curr_target.influence = 0;
-            else {
-                this.curr_target.influence -= di;
-                finished = false;
-            }
-
-            if (finished) {
-                this.manager.removeTarget(this.curr_target);
-                this.manager.removeTarget(this.new_target);
-
-                this.curr_mesh.dispose();
-                this.curr_mesh = this.new_mesh;
-                this.curr_mesh.setEnabled(true);
-            }
-
+            this.targets.forEach(
+                (target, n) => {
+                    if (n == this.new_n) {
+                        if (Math.abs(target.influence - 1) < di)
+                            target.influence = 1;
+                        else {
+                            target.influence += di;
+                            finished = false;
+                        }
+                    } else {
+                        if (Math.abs(target.influence) < di)
+                            target.influence = 0;
+                        else {
+                            target.influence -= di;
+                            finished = false;
+                        }
+                    }
+                }
+            );
 
             /*if (finished) {
                 for (let n = 0; n < Spiral_Top.config_len; n++) {
@@ -178,8 +158,6 @@ class SpiralView implements ISpiralParams {
     }
 
     click_handler(p, pick) {
-        if(this.do_transition) return;
-
         const screenWidth = window.innerWidth;
         const screenHeight = window.innerHeight;
         const screenCenterX = screenWidth / 2;
@@ -196,19 +174,14 @@ class SpiralView implements ISpiralParams {
         let n = Math.round(scaledDistance);
         n = Math.min(n, Spiral_Top.all_configs.length - 1);
         if (n != this.new_n) {
-            this.new_n = n;
-
-            this.curr_target = MorphTarget.FromMesh(this.curr_mesh, `spiral_${this.curr_n}`, 1);
-            this.new_mesh = this.create_spiral_mesh(n, this.spirals[n], false);
-            this.new_target = MorphTarget.FromMesh(this.new_mesh, `spiral_${this.new_n}`, 0);
-            this.manager.addTarget(this.curr_target);
-            this.manager.addTarget(this.new_target);
+            this.create_spiral_mesh(n, this.spirals[n]);
             this.curr_n = this.new_n;
+            this.new_n = n;
             this.do_transition = true;
             this.gui.m1_changed();
         }
-        // console.log(n, Spiral_Top.all_configs.length, this.targets.map(t => t.influence), this.do_transition, this.spirals[this.new_n].m1);
-        // this.camera.autoRotationBehavior.resetLastInteractionTime(0);
+        console.log(n, Spiral_Top.all_configs.length, this.targets.map(t => t.influence), this.do_transition, this.spirals[this.new_n].m1);
+        this.camera.autoRotationBehavior.resetLastInteractionTime(0);
 
 
         if (pick.hit) {
